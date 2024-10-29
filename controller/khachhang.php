bf094a3e81601f9e2941e15a5345bd97eaff87dd
HThinhDC96 <thinhdc2196n527@vlvh.ctu.edu.vn>
2024-10-30 01:38:55 +0700

    khach hang - dang ky - kiem tra trung email

--- controller/khachhang.php
+++ controller/khachhang.php
@@ -1,6 +1,6 @@
-<?php 
-    include_once '../../lib/session.php';
-    include '../../lib/database.php';
+<?php
+include_once '../../lib/session.php';
+include '../../lib/database.php';
 ?>
 <?php
 class khachhang
@@ -34,20 +34,46 @@ class khachhang
         $hoten = $data['hoten'];
         $email = $data['email'];
         $password = $data['password'];
-        $query = "INSERT INTO khachhang(hoten, email, matkhau) VALUES ('$hoten', '$email', '$password')";
-        $result = $this->db->insert($query);
-        if ($result) {
-            ?>
-            <script>alert("Đăng ký thành công!")</script>
-            <?php
-            header("Location: login.php");
-            return true;
-        } else {
-            $alert = "Đăng ký không thành công!";
-            ?>
-            <script>alert("Đăng ký không thành công! Thử lại...")</script>
-            <?php
-            return $alert;
+        try {
+            if (!$this->getByEmail($email)) {
+                $query = "INSERT INTO khachhang(hoten, email, matkhau) VALUES ('$hoten', '$email', '$password')";
+                $result = $this->db->insert($query);
+                if ($result) {
+                    ?>
+                    <script>alert("Đăng ký thành công!")</script>
+                    <?php
+                    $alert = "Đăng ký không thành công!";
+                    header("Location: login.php");
+                    // return $alert;
+                } else {
+                    $alert = "Đăng ký không thành công!";
+                    ?>
+                    <script>alert("Đăng ký không thành công! Thử lại...")</script>
+                    <?php
+                    return $alert;
+                }
+            } else {
+                $alert = "Email đã được đăng ký. Nhập Email khác hoặc đăng nhập!";
+                return $alert;
+            }
+        } catch (Exception) {
+            echo "Có lỗi xảy ra. Vui lòng thử lại sau.";
         }
+    }
+
+    // Lay toan bo khach hang
+    function getAll()
+    {
+        $query = "SELECT Email FROM KhachHang";
+        $result = $this->db->select($query);
+        return $result;
+    }
+
+    // Check khach hang dang ky (da dang ky chua)
+    function getByEmail($email)
+    {
+        $query = "SELECT * FROM KhachHang WHERE Email = '$email'";
+        $result = $this->db->select($query);
+        return $result;
     }
 }
